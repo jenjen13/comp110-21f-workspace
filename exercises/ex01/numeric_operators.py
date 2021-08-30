a291134bdd3fa8e9943aeb0dda24eabe3c94e57d
Jennifer Tran <jenjen1@email.unc.edu>
2021-08-29 21:36:22 -0400

    Progress on Exercise 1

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -1,10 +1,10 @@
 """This program computes exponents, division, integer division, and remainder modulo."""
 __author__ = "730404596"
 
-left_side = int(input("Left-hand side: "))
-right_side = int(input("Right-hand side: "))
+left_side: int = int(input("Left-hand side: "))
+right_side: int = int(input("Right-hand side: "))
 ans_one = int(left_side ** right_side)
-ans_two = int(left_side / right_side)
+ans_two = float(left_side / right_side)
 ans_three = int(left_side // right_side)
 ans_four = int(left_side % right_side)
 print(str(left_side) + " " + "**" + " " + str(right_side) + " " + "is" + " " + str(ans_one))
