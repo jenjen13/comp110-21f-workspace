e8a6cfbe5e6f2c83299e619d6709d24c77fb3990
Jennifer Tran <jenjen1@email.unc.edu>
2021-09-15 23:17:11 -0400

    Progress on Exercise 3

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -5,13 +5,15 @@ __author__ = "730404596"
 word: str = input("Enter a word: ")
 letter: str = ""
 i: int = 0
-dup: str = ""
-huh: str = ""
+g: int = 0
 
-while i < (len(word)) - 1:
+while i < (len(word)):
     letter = word[i]
-    dup = word[i + 1]
-    if letter == dup:
-        print("true")
-    print("false")
+    g = i + 1
+    while g < (len(word)): 
+        if letter == word[g]:
+            print("Found duplicate: True")
+        else:
+            print("Found duplicate: False")
+        g = g + 1
     i = i + 1
