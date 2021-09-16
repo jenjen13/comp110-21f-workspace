812d0cd8ea906f6c347c8dc950dbd1f258bbef60
Jennifer Tran <jenjen1@email.unc.edu>
2021-09-15 20:16:07 -0400

    Progress on Exercise 3

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,17 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730404596"
+
+word: str = input("Enter a word: ")
+letter: str = ""
+i: int = 0
+dup: str = ""
+huh: str = ""
+
+while i < (len(word)) - 1:
+    letter = word[i]
+    dup = word[i + 1]
+    if letter == dup:
+        print("true")
+    print("false")
+    i = i + 1
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,15 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730404596"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+depth: int = int(input("Depth: "))
+second: str = str()
+counter: int = 0
+
+
+while counter < depth:
+    second = second + TREE 
+    print(second)
+    counter = counter + 1
