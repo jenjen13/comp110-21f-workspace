40cadbe3581be4d97d8ba9d48949ea902bbf9068
Jennifer Tran <jenjen1@email.unc.edu>
2021-09-28 21:07:06 -0400

    Progress on Exercise 4

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,35 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730404596"
 
 
 # TODO: Implement your functions here.
+
+def all(ex: list[int], lol: int) -> bool:
+    """Find ints."""
+    i: int = 0 
+    while i < (len(ex)):
+        if ex[i] != lol:
+            return False
+        i += 1
+    return True
+
+
+def is_equal(ki: list[int], bo: list[int]) -> bool:
+    """Given two lists of ints, return True if every elemtn is equal."""
+    if ki != bo:
+        return False
+    return True
+
+
+def max(lop: list[int]) -> int:
+    """Given a list of ints, max should return the largest in list."""
+    i: int = 0
+    if len(lop) == 0:
+        raise ValueError("max() arg is an empty List")
+    else:
+        while i < len(lop):
+            if lop[i] > lop[i - 1]:
+                return lop[i]
+            i += 1
+    return lop[i]
